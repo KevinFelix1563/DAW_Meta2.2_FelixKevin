b234047cfc4d7d50a0d0b0d55e1384d4dc095d79
Kevin Felix <kevin.felix59@uabc.edu.mx>
2026-03-05 00:53:09 -0800

    fix: AppFooter muy largo

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,15 +1,20 @@
 <template>
-  <v-footer color="grey-darken-4" class="text-white py-4" app>
+  <v-footer color="grey-darken-4" class="text-white py-2" app>
     <v-container>
-      <v-row justify="center">
-        <v-col cols="12" sm="8" md="6" class="text-center">
-          <p class="mb-1 font-weight-bold">Kevin Yassir Felix Sanchez</p>
-          <p class="mb-1">Matrícula: 1196791</p>
-          <p class="mb-1">Fecha actual: {{ currentDate }}</p>
-          <p class="text-caption mt-2 text-grey-lighten-1">
-            © {{ currentYear }} - kevin.felix59@uabc.edu.mx
-          </p>
+      <v-row justify="space-between" align="center" no-gutters>
+        
+        <v-col cols="12" md="4" class="text-center text-md-left mb-1 mb-md-0 text-body-2">
+          <strong>Kevin Yassir Felix Sanchez</strong> • 1196791
         </v-col>
+        
+        <v-col cols="12" md="4" class="text-center mb-1 mb-md-0 text-caption text-grey-lighten-1">
+          © {{ currentYear }} - kevin.felix59@uabc.edu.mx
+        </v-col>
+
+        <v-col cols="12" md="4" class="text-center text-md-right text-caption text-grey-lighten-1">
+          Fecha actual: {{ currentDate }}
+        </v-col>
+
       </v-row>
     </v-container>
   </v-footer>
@@ -18,8 +23,6 @@
 <script setup>
 import { ref, computed } from 'vue';
 
-// Se obtiene la fecha actual usando el objeto Date() de JS y la propiedad computed
-// de Vue para indicar que se debe actualizar
 const date = new Date();
 const currentDate = ref(date.toLocaleDateString('es-MX'));
 const currentYear = computed(() => date.getFullYear());
